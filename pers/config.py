4b0b52a1f5866e920db90855fe2f9cb7d449416a
orensheffer7 <orensheffer7@gmail.com>
2021-02-17 10:41:53 +0200

    Update config.py
    Hide Client secret

--- pers/config.py
+++ pers/config.py
@@ -7,7 +7,7 @@ REPO_NAME = 'TRUSTIT'
 ### If not entered github api is limited to 100 calls a day ###
 
 CLIENT_ID = 'e93b61d392e5682e0400'
-CLIENT_SECRET = '6474d747e10de48cd0609978ed8ad3babfdda57c'
+CLIENT_SECRET = '*******************************'
 
 
 ### PATHS ###
